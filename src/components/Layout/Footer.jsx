aff871457270cf7608f9a4299b2b24456fb8f280
hiro-engineer4 <thejunkcocoa@gmail.com>
2023-07-26 12:51:06 +0900

    fix

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from "./Footer.module.css";
 
 export function Footer() {
-  return <footer className={styles.footer}>© 2022 cocoa-engineer</footer>;
+  return <footer className={styles.footer}>© 2022 hiro-engineer</footer>;
 }
